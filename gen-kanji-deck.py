c245c0679736d6f804bd4becd6fcf527dd89a45b
Ari Lamstein <arilamstein@gmail.com>
2015-04-24 23:36:15 -0700

    add grade level

--- gen-kanji-deck.py
+++ gen-kanji-deck.py
@@ -6,11 +6,10 @@ class Kanji:
 	The Kanji class contains a character plus metadata about that character
 	"""
 
-	def __init__(self, character):
+	def __init__(self, character, grade):
 		self.character      = character
+		self.grade          = grade
 		self.dictionary_url = "http://jisho.org/search/" + self.character + "%20%23kanji"
-
-		self.get_data_from_dictionary()
 
 	def get_jlpt_level(self, soup):
 		el = soup.find(attrs={'class': ('jlpt')})
@@ -33,45 +32,50 @@ class Kanji:
 		self.jlpt_level = self.get_jlpt_level(soup)
 		self.definition = self.get_definition(soup)
 
-		self.examples       = "c"
+		self.examples   = "c"
 
 	def as_csv(self):
 		""" 
 		Convert object to csv
 		"""
 		return ",".join([self.character, 
+						 self.grade,
 						 self.dictionary_url, 
 						 self.jlpt_level, 
 						 '\"' + self.definition + '\"', 
 						 self.examples]).encode('utf-8')
 
-def get_characters(): 
+def get_kanji_from_wikipedia(): 
 	"""
-	Gets a list of all joyo kanji characters from wikipedia
+	Gets a list of all joyo kanji, plus their grade level from wikipedia
 	For information on joyo kanji see: http://en.wikipedia.org/wiki/J%C5%8Dy%C5%8D_kanji
 	"""
 
 	joyo_kanji_url = "http://en.wikipedia.org/wiki/List_of_j%C5%8Dy%C5%8D_kanji"
 	soup = BeautifulSoup(urlopen(joyo_kanji_url), 'html.parser')
 
-	characters = []
+	kanji = []
 	# the kanji are in the 2nd column of this table
 	for row in soup.findAll('table')[2].findAll('tr'):
 		cells = row.findAll('td')
 		if len(cells) > 0: # i.e., ignore the header
 			if cells[1].a.contents:
-				characters.append(cells[1].a.contents[0])
-	
-	return characters
+				character = cells[1].a.contents[0]
+				grade     = cells[5].contents[0]
+
+			kanji.append(Kanji(character, grade))
+
+	return kanji
 
 # get all the joyo characters from wikipedia
-characters = get_characters()
+joyo_kanji = get_kanji_from_wikipedia()
 
 # write them as a csv file
 f = open("kanji.csv", "w")
-#i = 0
-for c in characters:
-	print >> f, Kanji(c).as_csv()
-#	i+=1
-#	if i > 10:
-#		break
+i = 0
+for k in joyo_kanji:
+	k.get_data_from_dictionary()
+	print >> f, k.as_csv()
+	i+=1
+	if i > 10:
+		break
